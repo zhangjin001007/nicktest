860f0db9e046904c90fb445508b363d18ab6ea97
Jin Zhang <zhangjin001007@hotmail.com>
2015-09-14 17:21:22 +0800

    Add version 1

---
+++ GithubTest/src/MyTest1.java
@@ -0,0 +1,13 @@
+
+public class MyTest1 {
+
+    public MyTest1() {
+	// TODO Auto-generated constructor stub
+    }
+
+    public static void main(String[] args) {
+	// TODO Auto-generated method stub
+
+    }
+
+}
