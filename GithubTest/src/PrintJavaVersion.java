02d923d1a34370af60a91f982cdee7ff9135fa9c
zhangjin001007 <zhangjin001007@hotmail.com>
2016-05-30 12:35:57 +0800

    updated!

--- GithubTest/src/PrintJavaVersion.java
+++ GithubTest/src/PrintJavaVersion.java
@@ -2,6 +2,6 @@
 public class PrintJavaVersion {
     public static void main(String[] args) {
 	// TODO Auto-generated method stub
-    	System.out.println("1: hello jenkins!! updated!");
+    	System.out.println(System.getProperty("java.version"));
     }
 }
